a3e94fe3661d1a24abaf20e15159b7be557e496a
Quan Le <75654249+anhquanle@users.noreply.github.com>
2026-05-07 11:40:16 +0700

    Sidebar

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -35,7 +35,8 @@ export const Heading = () => {
       {!isAuthenticated && !isLoading && (
         <SignInButton mode='modal'>
           <Button>
-            Get Jotion free <ArrowRight className='h-4 w-4 ml-2' />
+            Get Jotion free
+            <ArrowRight className='h-4 w-4 ml-2' />
           </Button>
         </SignInButton>
       )}
